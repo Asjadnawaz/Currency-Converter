fa6b3f78fb75ba11001913cd5a5eb2424de0756d
Asjad Nawaz Khan <asjadnawaz2002@gmail.com>
2024-04-13 18:28:11 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+let currency = {
+    USD: 1,
+    PKR: 277,
+    INR: 75
+};
+let user_answer = await inquirer.prompt([{
+        message: "Select From Curreny ",
+        type: "list",
+        name: "From",
+        choices: ["USD", "PKR", "INR"]
+    },
+    {
+        message: "Select to Currency",
+        type: "list",
+        name: "to",
+        choices: ["USD", "PKR", "INR"]
+    },
+    {
+        message: "Enter Amount to convert",
+        type: "number",
+        name: "amount",
+    }
+]);
+let fromAmount = currency[user_answer.From];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let BaseAmount = amount / fromAmount;
+let convertedAmount = BaseAmount * toAmount;
+console.log(convertedAmount);
---
+++ index.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+
+let currency: any = {
+    USD: 1,
+    PKR: 277,
+    INR: 75
+}
+
+let user_answer = await inquirer.prompt ([{
+    message: "Select From Curreny ",
+    type: "list",
+    name: "From",
+    choices: ["USD", "PKR", "INR"]
+},
+{
+    message: "Select to Currency",
+    type: "list",
+    name: "to",
+    choices: ["USD", "PKR", "INR"]
+},
+
+{
+    message: "Enter Amount to convert",
+    type: "number",
+    name: "amount",
+}
+]);
+
+let fromAmount: any = currency[user_answer.From];
+let toAmount: any = currency[user_answer.to];
+let amount: any = user_answer.amount;
+let BaseAmount: any = amount / fromAmount;
+let convertedAmount: any = BaseAmount * toAmount;
+
+console.log(convertedAmount);
+
